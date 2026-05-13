3933eb8d40d3988f35806d99209fc940fc2c5b3c
João Almeida <toxicyszconta@gmail.com>
2026-05-12 21:23:19 -0400

    Implement program to print integers from 100 to 1

---
+++ Ex2.java
@@ -0,0 +1,14 @@
+// 02) Faça um programa que imprima todos os números inteiros de 100 a 1 (em ordem decrescente).
+
+public class Ex2 {
+    
+    public static void main(String[] args){
+        
+        
+        for (int i = 100; i > 0; i--){
+            System.out.println(i);
+        }
+        
+    }
+    
+}
