76cca61cc56a12bfd123419f4370e5e617d32c6b
João Almeida <toxicyszconta@gmail.com>
2026-05-12 21:22:59 -0400

    Display numbers from 1 to 300
    Fix the loop condition to iterate correctly from 1 to 300.

---
+++ Ex1.java
@@ -0,0 +1,13 @@
+// 01) Elabore um programa que mostre os números de 1 a 300.
+
+public class Ex1 {
+    
+    public static void main(String[] args){
+        
+        for (int i = 1; i < 300 + 1; i++){
+            System.out.println(i);
+        }
+        
+    }
+    
+}
