61517e6fc21c0d42261d7cea3d3fc56aef340d43
João Almeida <toxicyszconta@gmail.com>
2026-05-12 21:25:20 -0400

    Implement loop to print multiples of 7
    Fix the loop condition to iterate correctly up to 2854.

---
+++ Ex6.java
@@ -0,0 +1,13 @@
+public class Ex6 {
+
+    public static void main(String[] args){
+        
+        for (int i = 1; i < 2854 + 1; i++){
+            if ( i % 7 == 0){
+                System.out.println(i + " É multiplo de 7.");
+            }
+        }
+        
+    }
+    
+}
